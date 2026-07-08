2f09a6f6db9025fdd26b1c89555fc596462e4836
Thanh Nhan Tong <thanhnhantong03@gmail.com>
2026-07-07 20:42:57 -0500

    modified STrack constructor to include classIDs, included into algorithm project

---
+++ Portal2TurretVS/Tracking/ByteTrack-CSharp/Object.cs
@@ -0,0 +1,16 @@
+﻿namespace ByteTrackCSharp;
+
+public class Object
+{
+    //this is a buffer container
+    public Rect rect;
+    public int label;
+    public float prob; //conf scores
+
+    public Object(Rect rect, int label, float prob)
+    {
+        this.rect = new Rect(rect);
+        this.label = label;
+        this.prob = prob;
+    }
+}
